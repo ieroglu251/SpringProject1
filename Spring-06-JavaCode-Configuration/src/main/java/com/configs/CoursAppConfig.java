f9e9f0a25c4145f7d0ca6940c91692e584aa7b23
ieroglu251 <ieroglu251@gmail.com>
2020-10-24 13:22:31 -0500

    configuration application

---
+++ Spring-06-JavaCode-Configuration/src/main/java/com/configs/CoursAppConfig.java
@@ -0,0 +1,11 @@
+package com.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com")
+public class CoursAppConfig {
+
+
+}
