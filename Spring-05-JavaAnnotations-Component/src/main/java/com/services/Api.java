0800877fc3e5fa6914a40b019fae6b681d1ae970
ieroglu251 <ieroglu251@gmail.com>
2020-10-24 10:08:43 -0500

    component annotation

---
+++ Spring-05-JavaAnnotations-Component/src/main/java/com/services/Api.java
@@ -0,0 +1,14 @@
+package com.services;
+
+import com.interfaces.Course;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Api implements Course {
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("API");
+
+    }
+}
---
+++ Spring-05-JavaAnnotations-Component/src/main/java/com/services/Java.java
@@ -0,0 +1,13 @@
+package com.services;
+
+import com.interfaces.Course;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Java implements Course {
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly teaching hours java 20");
+
+    }
+}
