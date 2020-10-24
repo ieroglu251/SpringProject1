514514bbadb7609f2af8acad15f8047fdf4c9d7c
ieroglu251 <ieroglu251@gmail.com>
2020-10-24 13:31:40 -0500

    Bean annotation

--- Spring-06-JavaCode-Configuration/src/main/java/com/configs/CoursAppConfig.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/configs/CoursAppConfig.java
@@ -1,5 +1,8 @@
 package com.configs;
 
+import com.services.Java;
+import com.services.Selenium;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,5 +10,13 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("com")
 public class CoursAppConfig {
 
+    @Bean
+public Java java(){
+    return new Java();
+}
 
+@Bean
+public Selenium selenium(){
+    return new Selenium();
+}
 }
--- Spring-06-JavaCode-Configuration/src/main/java/com/services/Selenium.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/services/Selenium.java
@@ -3,7 +3,7 @@ package com.services;
 import com.interfaces.Course;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class Selenium implements Course {
     @Override
     public void getTeachingHours() {
