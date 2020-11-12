a2154babc7a0dc78b700b6aeab54f0060e7c7554
ieroglu251 <ieroglu251@gmail.com>
2020-11-12 16:51:05 -0600

    MVC-Controller

--- Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cyb/controller/HomeController.java
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cyb/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.cyb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,7 +23,7 @@ public class HomeController {
         return "home";
     }
 
-    @PostMapping("/1")
+    @PostMapping("/2")
     public String getRequestMapping2(){
 
 
@@ -32,6 +33,14 @@ public class HomeController {
     @RequestMapping("/3")
     public String getRequestMapping3(){
 
+
+        return "home";
+    }
+
+    @GetMapping("/1/{name}")
+    public String variablePath(@PathVariable("name") String name){
+
+        System.out.println( "name is" + name);
 
         return "home";
     }
---
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cyb/controller/MentorController.java
@@ -0,0 +1,15 @@
+package com.cyb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+    @GetMapping("/show")
+    public String getShow(){
+        return "home";
+    }
+}
