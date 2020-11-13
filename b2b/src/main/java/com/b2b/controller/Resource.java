76a34374dafcd0eb5660db8d4deed333328cb871
ieroglu251 <ieroglu251@gmail.com>
2020-11-12 23:41:48 -0600

    Back to Basics

---
+++ b2b/src/main/java/com/b2b/controller/Resource.java
@@ -0,0 +1,16 @@
+package com.b2b.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class Resource {
+
+    @GetMapping("home")
+    public String lendingPage(){
+
+        return "index";
+    }
+}
