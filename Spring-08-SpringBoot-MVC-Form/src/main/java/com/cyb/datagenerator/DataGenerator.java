0f0b84ee7885e9c63cba0a18a60876f011597476
ieroglu251 <ieroglu251@gmail.com>
2020-11-21 01:26:15 -0600

    MVC forms

---
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cyb/datagenerator/DataGenerator.java
@@ -0,0 +1,18 @@
+package com.cyb.datagenerator;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DataGenerator {
+
+    public static List<String> getStateList(){
+
+        List<String> stateList = Arrays.asList("tx",
+                "NW",
+                "AL",
+                "AK");
+
+return stateList;
+
+    }
+}
