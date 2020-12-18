baecc6ba51e6a3f76865a1ac909334fe66136beb
ieroglu251 <ieroglu251@gmail.com>
2020-12-17 22:20:19 -0600

    many to one

---
+++ Sprint-09-ORM-Mapping-ManyToOne/src/main/java/com/cyb/entity/Employee.java
@@ -0,0 +1,50 @@
+package com.cyb.entity;
+
+import com.cyb.enums.Gender;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Employee extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Column(columnDefinition = "Date")
+    private LocalDate hireDate;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private int salary;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE,CascadeType.PERSIST})
+    @JoinColumn(name = "ismail")
+    private Department department;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+
+
+    public Employee(String firstName, String lastName, String email, LocalDate hireDate, Gender gender, int salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.hireDate = hireDate;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+
+}
---
+++ Sprint-09-ORM-Mapping-ManyToOne/src/main/java/com/cyb/enums/Gender.java
@@ -0,0 +1,5 @@
+package com.cyb.enums;
+
+public enum Gender {
+    M,F
+}
