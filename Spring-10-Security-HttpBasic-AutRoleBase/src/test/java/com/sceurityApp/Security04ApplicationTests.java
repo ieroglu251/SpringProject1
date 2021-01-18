83a3c4a69ecdaaa8e9f87a846eaf51032abe9527
ieroglu251 <ieroglu251@gmail.com>
2021-01-17 23:36:02 -0600

    database security

---
+++ Spring-10-Security-HttpBasic-AutRoleBase/src/test/java/com/sceurityApp/Security04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sceurityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Security04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
