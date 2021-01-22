97f2137261378d8fca9501cdd8184341540f60c4
ieroglu251 <ieroglu251@gmail.com>
2021-01-22 06:50:20 -0600

    UI auth

--- Spring-10-Security-FormAuth/src/main/java/com/cyb/security/SecurityConfiguration.java
+++ Spring-10-Security-FormAuth/src/main/java/com/cyb/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.cyb.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserPrincipalDetailsService userPrincipalDeatailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -30,7 +34,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login?logout=true");
+                .logoutSuccessUrl("/login?logout=true")
+                .and()
+                .rememberMe()
+                .tokenValiditySeconds(120)
+                .key("cyb")
+                .userDetailsService(userPrincipalDeatailsService);
 
     }
 
