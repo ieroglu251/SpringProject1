56ea4f44ae89bf668c621335d6b3d5eacecba9c3
ieroglu251 <ieroglu251@gmail.com>
2021-02-27 21:07:58 -0600

    Exception handling message

--- Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/exception/ExceptionMessageHandler.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/exception/ExceptionMessageHandler.java
@@ -1,4 +1,33 @@
 package com.cybertek.exception;
 
+import com.cybertek.entity.ResponseWrapper;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.nio.file.AccessDeniedException;
+
+@RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class ExceptionMessageHandler {
+
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<ResponseWrapper> serviceException(ServiceException se){
+        String message = se.getMessage();
+
+        return new ResponseEntity<>(ResponseWrapper.builder().sucess(false).code(HttpStatus.CONFLICT.value()).message(message).build(),HttpStatus.CONFLICT);
+
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ResponseWrapper> accessdeniedexception(AccessDeniedException se){
+        String message = se.getMessage();
+
+        return new ResponseEntity<>(ResponseWrapper.builder().sucess(false).code(HttpStatus.FORBIDDEN.value()).message(message).build(),HttpStatus.CONFLICT);
+
+    }
 }
