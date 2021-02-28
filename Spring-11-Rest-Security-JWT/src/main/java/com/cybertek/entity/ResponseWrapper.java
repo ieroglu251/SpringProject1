fe711434e04aaf329bad39ff3dea9d75a1460ecc
ieroglu251 <ieroglu251@gmail.com>
2021-02-27 21:28:58 -0600

    Exception handling message

--- Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/entity/ResponseWrapper.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/entity/ResponseWrapper.java
@@ -1,15 +1,13 @@
 package com.cybertek.entity;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Builder
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseWrapper {
